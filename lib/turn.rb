4cb7a861ec480615fb26583c83ce0f7f3c18b30f
Danny Brandt <dvbrandt90@gmail.com>
2020-12-01 04:52:20 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,50 @@
+ def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(input)
+  temp = input.to_i
+  index = temp - 1
+  return index
+end
+
+def position_taken?(board, input)
+  index = input_to_index(input)
+  check = board[index]
+  if check = "X" || check = "Y" || check = -1 
+    true 
+  else
+    false
+  end
+end
+
+def valid_move?(board, index)
+  
+  if !position_taken?(board, index) && index.between?(0,8)
+      true
+  else
+      false
+  end
+end
+
+def move(board,index,player_char ="X")
+  board[index] = player_char
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+    
+  if valid_move?(board, index) 
+    move(board, index, player_char = "X")
+    display_board(board)
+  else
+   puts "Please enter a valid move."
+    turn(board)
+    end
+end
